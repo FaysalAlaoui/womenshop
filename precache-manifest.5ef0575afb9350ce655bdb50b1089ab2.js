4c9cdfee31705c3cb6d0749d68b2fdaa255e5940
FaysalAlaoui <faysal.alaoui.inbox@gmail.com>
2019-08-28 03:33:11 +0000

    Updates

---
+++ precache-manifest.5ef0575afb9350ce655bdb50b1089ab2.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ea448a9eb85837b1ea374c326ac07dd",
+    "url": "/womenshop/index.html"
+  },
+  {
+    "revision": "5976f29080681d1f742c",
+    "url": "/womenshop/static/css/2.148343e1.chunk.css"
+  },
+  {
+    "revision": "7a6ed2346484c3ea3db9",
+    "url": "/womenshop/static/css/main.de5a8744.chunk.css"
+  },
+  {
+    "revision": "5976f29080681d1f742c",
+    "url": "/womenshop/static/js/2.2807ebb7.chunk.js"
+  },
+  {
+    "revision": "7a6ed2346484c3ea3db9",
+    "url": "/womenshop/static/js/main.5afcda4b.chunk.js"
+  },
+  {
+    "revision": "28b6be0ca2fd7b30108a",
+    "url": "/womenshop/static/js/runtime~main.d5783638.js"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/womenshop/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/womenshop/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/womenshop/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/womenshop/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/womenshop/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/womenshop/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/womenshop/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/womenshop/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/womenshop/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/womenshop/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/womenshop/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/womenshop/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/womenshop/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/womenshop/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/womenshop/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/womenshop/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/womenshop/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/womenshop/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/womenshop/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/womenshop/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/womenshop/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/womenshop/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "38975343b586296e9b73e6b56cc3ec5d",
+    "url": "/womenshop/static/media/fa-brands-400.38975343.svg"
+  },
+  {
+    "revision": "8e49b728413079dfd9ee45d0c58d54e4",
+    "url": "/womenshop/static/media/fa-brands-400.8e49b728.eot"
+  },
+  {
+    "revision": "9f018d1025561e96439f7c0e9026301a",
+    "url": "/womenshop/static/media/fa-brands-400.9f018d10.woff"
+  },
+  {
+    "revision": "9f4ce3dc689981a1b87faab0f5484f9e",
+    "url": "/womenshop/static/media/fa-brands-400.9f4ce3dc.woff2"
+  },
+  {
+    "revision": "b7d071b9c3c197bff4af902070622423",
+    "url": "/womenshop/static/media/fa-brands-400.b7d071b9.ttf"
+  },
+  {
+    "revision": "7980a6361c25b4665dbbe92d4488783c",
+    "url": "/womenshop/static/media/fa-regular-400.7980a636.woff2"
+  },
+  {
+    "revision": "7aaf5675efd6339e9aba53ecbe5f1e36",
+    "url": "/womenshop/static/media/fa-regular-400.7aaf5675.woff"
+  },
+  {
+    "revision": "859001f6ae8eb0bb3878aaa971b50fc6",
+    "url": "/womenshop/static/media/fa-regular-400.859001f6.eot"
+  },
+  {
+    "revision": "da8a235bb207c74eea21507f3a86a53b",
+    "url": "/womenshop/static/media/fa-regular-400.da8a235b.svg"
+  },
+  {
+    "revision": "f33342516f7cbe46f1d6b68f9e7bbeda",
+    "url": "/womenshop/static/media/fa-regular-400.f3334251.ttf"
+  },
+  {
+    "revision": "0be94a07755ba9b88f2ebcac0f23a3da",
+    "url": "/womenshop/static/media/fa-solid-900.0be94a07.woff"
+  },
+  {
+    "revision": "64b3e814a66c2719b15abf8f7998bd73",
+    "url": "/womenshop/static/media/fa-solid-900.64b3e814.woff2"
+  },
+  {
+    "revision": "7726a281c1d436eb038f78c6e9048c96",
+    "url": "/womenshop/static/media/fa-solid-900.7726a281.svg"
+  },
+  {
+    "revision": "e2675a616b68f446fa6284c111554c7f",
+    "url": "/womenshop/static/media/fa-solid-900.e2675a61.eot"
+  },
+  {
+    "revision": "f14c3b2ff7c821a4c838debbffd6ad2d",
+    "url": "/womenshop/static/media/fa-solid-900.f14c3b2f.ttf"
+  },
+  {
+    "revision": "e513745c62870efb700fd47cbac12763",
+    "url": "/womenshop/static/media/mens-cloathes-pic1.e513745c.png"
+  },
+  {
+    "revision": "0741adc30468427630d4aa875cc784f5",
+    "url": "/womenshop/static/media/mens-cloathes-pic2.0741adc3.png"
+  },
+  {
+    "revision": "8bdb6d33661e1dd9122f14bf3dfd48c7",
+    "url": "/womenshop/static/media/women-cloathes-braided-espadrille-wedges-pic1.8bdb6d33.png"
+  },
+  {
+    "revision": "a1033bd8cfa6fd25f9ff4280c5d7461e",
+    "url": "/womenshop/static/media/women-cloathes-faux-leather-heals-pic1.a1033bd8.png"
+  },
+  {
+    "revision": "877f40327d6fdddea3c3504814e08829",
+    "url": "/womenshop/static/media/women-cloathes-faux-leather-pointed-toe-booties-pic1.877f4032.png"
+  },
+  {
+    "revision": "fdace54387ea87299e60ff48de465ed5",
+    "url": "/womenshop/static/media/women-cloathes-faux-suede-block-heels-pic1.fdace543.png"
+  },
+  {
+    "revision": "2d4fde8c5aaaf0affa11085de6921500",
+    "url": "/womenshop/static/media/women-cloathes-keds-metallic-platform-sneakers-pic1.2d4fde8c.png"
+  },
+  {
+    "revision": "3f728cae433f51c1a5c2b25094219a9e",
+    "url": "/womenshop/static/media/women-cloathes-layered-pendant-necklac-pic1.3f728cae.png"
+  },
+  {
+    "revision": "2ded332a6a78d1554752824fe86392c4",
+    "url": "/womenshop/static/media/women-cloathes-layered-pendant-necklac-pic2.2ded332a.png"
+  },
+  {
+    "revision": "7a6366f01f0384b7753a53a50bdbb510",
+    "url": "/womenshop/static/media/women-cloathes-low-top-lace-up-sneakers-pic1.7a6366f0.png"
+  },
+  {
+    "revision": "6f5dbd7678e2146f790991feab612200",
+    "url": "/womenshop/static/media/women-cloathes-ombre-ribbed-mock-neck-top-and-skirt-pic1.6f5dbd76.png"
+  },
+  {
+    "revision": "83b6d1f22090cfc8e0e7b9461629597e",
+    "url": "/womenshop/static/media/women-cloathes-ombre-tinted-sunglasses-pic1.83b6d1f2.png"
+  },
+  {
+    "revision": "627c219ad3369262229e4540967a2829",
+    "url": "/womenshop/static/media/women-cloathes-ombre-tinted-sunglasses-pic2.627c219a.png"
+  },
+  {
+    "revision": "0ba0209b241f0da97de1d12813ec22f8",
+    "url": "/womenshop/static/media/women-cloathes-plussize-striped-trim-pullover-pic1.0ba0209b.png"
+  },
+  {
+    "revision": "09ff90b13c049423954e533233abdf80",
+    "url": "/womenshop/static/media/women-cloathes-quilted-structured-crossbody-bag-pic1.09ff90b1.png"
+  },
+  {
+    "revision": "f2fd374a294e56ac07dd34669f5e4b19",
+    "url": "/womenshop/static/media/women-cloathes-rhinestone-hoop-earrnings-pic1.f2fd374a.png"
+  },
+  {
+    "revision": "146f137a454c8d446c7104e151d11ef5",
+    "url": "/womenshop/static/media/women-cloathes-rhinestone-hoop-earrnings-pic2.146f137a.png"
+  },
+  {
+    "revision": "70419bc84a86909b1b95ee19dc154d8f",
+    "url": "/womenshop/static/media/women-cloathes-sheer-floral-organza-shirt-pic1.70419bc8.png"
+  },
+  {
+    "revision": "4822848da482b35f8b1c88bea33968e6",
+    "url": "/womenshop/static/media/women-cloathes-sheer-floral-organza-shirt-pic2.4822848d.png"
+  },
+  {
+    "revision": "e72fcfe8b0a16d2323c1102925517c31",
+    "url": "/womenshop/static/media/women-cloathes-strappy-stiletto-heels-pic1.e72fcfe8.png"
+  }
+]);
